5824ad61f2601b0038d598673433ad8d57734a08
Simona Teodorowicz <saaamanta01@gmail.com>
2026-04-01 12:16:21 +0200

    add database integration

--- backend/auth_service/models.py
+++ backend/auth_service/models.py
@@ -1,16 +1,14 @@
-from database import engine
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import declarative_base
+from database import engine
 
 Base = declarative_base()
 
-
 class User(Base):
     __tablename__ = "users"
-
+    
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, unique=True)
     password = Column(String)
-
 
 Base.metadata.create_all(bind=engine)
